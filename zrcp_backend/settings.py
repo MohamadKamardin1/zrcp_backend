c1aa18d2e70efd7eb424085f8c12fd52077617e2
MohamadKamardin1 <focusinzanzibar@outlook.com>
2025-09-08 07:08:23 -0400

    Add Cloudinary for media file storage

--- zrcp_backend/settings.py
+++ zrcp_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!po28bv(0%w=q*4!&tjf_3n4th#jmnyup^0gz6n@26uq%#=)mb
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = [
     'localhost',
